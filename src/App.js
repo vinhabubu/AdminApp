8fec8008c0508f905112675043dbbcdd1956de73
Nguyễn Vinh <77477174+vinhabubu@users.noreply.github.com>
2023-06-27 22:34:10 +0700

    fix: add login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 import './App.css';
 import Home from './pages/home/Home';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate , Outlet} from 'react-router-dom';
 import UserList from './pages/userList/UserList';
 import User from './pages/user/User';
 import NewUser from './pages/newUser/NewUser';
@@ -12,7 +12,14 @@ import NewProduct from './pages/newProduct/NewProduct';
 import Login from './pages/login/Login';
 
 function App() {
-  const user = true;
+  const user = false;
+
+  const SidebarLayout = () => (
+    <>
+      <Sidebar />
+      <Outlet />
+    </>
+  );
   return (
     <BrowserRouter>
       {/* <Route path="/login" element={user ? <Navigate to="/" /> : <Login />}></Route> */}
@@ -20,9 +27,12 @@ function App() {
       <>
         <Topbar />
         <div className='container'>
-          <Sidebar />
+          {/* <Sidebar /> */}
 
           <Routes>
+            <Route path="/login" element={user ? <Navigate to="/" /> : <Login />}></Route> 
+            <Route element={<SidebarLayout />}>
+              
             <Route exact path='/' element={<Home />} />
 
             <Route path='/users' element={<UserList />} />
@@ -35,6 +45,7 @@ function App() {
 
             <Route path='/product/:productId' element={<Product />} />
             <Route path='/newproduct' element={<NewProduct />} />
+            </Route>
           </Routes>
         </div>
       </>
